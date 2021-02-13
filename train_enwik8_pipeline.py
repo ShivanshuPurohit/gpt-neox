285fa7ffbbd85d3d12647eb779ac8294da1ee87f
Shivanshu Purohit <42869065+ShivanshuPurohit@users.noreply.github.com>
2021-02-13 17:03:07 +0530

    Update train_enwik8_pipeline.py

--- train_enwik8_pipeline.py
+++ train_enwik8_pipeline.py
@@ -7,11 +7,14 @@ import deepspeed
 import torch
 from torch.utils.data import DataLoader
 from tqdm.auto import trange
+import wandb
+import socket
+from wandb import UsageError
 
 from gpt_neox import (GPTNeoX, AutoregressiveWrapper, TextSamplerDataset,
                       cycle, prepare_optimizer_parameters, decode_tokens, prepare_data,
                       GPTNeoX_Pipe)
-from gpt_neox.utils import is_main, get_args, get_params, load_ds_checkpoint, save_ds_checkpoint
+from gpt_neox.utils import is_main, get_args, get_params, load_ds_checkpoint, save_ds_checkpoint, get_wandb_api_key
 
 from gpt_neox.data_utils import read_enwik8_data
 import gpt_neox
@@ -59,6 +62,21 @@ if __name__ == '__main__':
         activation_checkpoint_interval=params.get('activation_checkpoint_interval', 1)
     )
 
+    ## Wandb
+    use_wandb = get_wandb_api_key() is not None
+    if use_wandb:
+        # only display system stats from one worker per machine
+        wandb_settings = wandb.Settings() if is_main(train_args) else wandb.Settings(_disable_stats=True)
+        name = f'{socket.gethostname()}-{train_args.local_rank}' if train_args.group_name else None
+
+        try:
+            wandb.init(project="neox_train_enwik8_pipeline", group=train_args.group_name, name=name, save_code=True, force=False,
+                       entity=params.get('wandb', {}).get('team'), settings=wandb_settings)
+        except UsageError as e:
+            use_wandb = False
+            print(e)
+            print('Skipping wandb. Execute `wandb login` on local machine to enable.')
+
     # prepare enwik8 data
     dset_params = params["dataset"]
     prepare_dataset(dset_params, train_args)
@@ -70,7 +88,7 @@ if __name__ == '__main__':
     # optimizer
     ds_model_params = prepare_optimizer_parameters(model)
     optim = torch.optim.Adam(ds_model_params, lr=params["learning_rate"])
-    
+
     # deepspeed loader
     model, optim, train_loader, _ = deepspeed.initialize(args=train_args,
                                                                 model=model,
@@ -78,12 +96,21 @@ if __name__ == '__main__':
                                                                 model_parameters=ds_model_params,
                                                                 training_data=train_dataset)
 
+    if use_wandb:
+        wandb.config.update(params)
+        wandb.watch(model, log_freq=10, log=params.get('wandb', {}).get('watch_model'))
+
     configure_checkpointing(model)
     current_iteration = load_ds_checkpoint(model, params, iteration=None)
+
     pbar = trange(current_iteration, params.get('train_steps', 100000), mininterval=10., desc='Training Model', dynamic_ncols=True)
     for i in pbar:
         loss = model.train_batch()
         pbar.set_description(f'Training Loss: {loss.item():.4f}')
         pbar.update()
+
         if not i % params.get('checkpoint_save_frequency', 1000) and i != 0:
             save_ds_checkpoint(i, model, params, params.get('keep_n_latest_checkpoints', 5), IS_MAIN)
+
+        if use_wandb:
+            wandb.log({'loss': loss.item()})
