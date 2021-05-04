8d05c9b348666fe738447682edd9287ac1b46eb7
Shivanshu Purohit <42869065+ShivanshuPurohit@users.noreply.github.com>
2021-05-04 14:23:58 +0530

    Update optimizers.py

--- megatron/optimizers.py
+++ megatron/optimizers.py
@@ -2,6 +2,67 @@ import torch
 from torch.optim import Optimizer
 
 
+def _compute_sparse_update(beta, acc, grad_values, grad_indices):
+    # In the sparse case, a single accumulator is used.
+    update_values = torch.gather(acc, 0, grad_indices[0])
+    if beta > 0.:
+        update_values.mul_(beta)
+    update_values.addcmul_(grad_values, grad_values, value=1. - beta)
+    return update_values
+
+
+def _compute_update(beta, acc_list, grad):
+    rank = len(acc_list)
+    update = acc_list[0].clone()
+    for i in range(1, rank):
+        # We rely on broadcasting to get the proper end shape.
+        update = torch.min(update, acc_list[i])
+    if beta > 0.:
+        update.mul_(beta)
+    update.addcmul_(grad, grad, value=1. - beta)
+    return update
+
+
+def _key(i):
+    # Returns key used for accessing accumulators
+    return 'accumulator_' + str(i)
+
+
+def _add_initial_accumulators(state, grad):
+    # Creates initial accumulators. For a dense tensor of shape (n1, n2, n3),
+    # then our initial accumulators are of shape (n1, 1, 1), (1, n2, 1) and
+    # (1, 1, n3). For a sparse tensor of shape (n, *), we use a single
+    # accumulator of shape (n,).
+    shape = grad.shape
+    rank = len(shape)
+    defaults = {'device': grad.device, 'dtype': grad.dtype}
+    acc = {}
+
+    if grad.is_sparse:
+        acc[_key(0)] = torch.zeros(shape[0], **defaults)
+    elif rank == 0:
+        # The scalar case is handled separately
+        acc[_key(0)] = torch.zeros(shape, **defaults)
+    else:
+        for i in range(rank):
+            acc_shape = [1] * i + [shape[i]] + [1] * (rank - 1 - i)
+            acc[_key(i)] = torch.zeros(acc_shape, **defaults)
+    state.update(acc)
+
+
+def _max_reduce_except_dim(tensor, dim):
+    # Computes max along all dimensions except the given dim.
+    # If tensor is a scalar, it returns tensor.
+    rank = len(tensor.shape)
+    result = tensor
+    if rank > 0:
+        assert dim < rank
+        for d in range(rank):
+            if d != dim:
+                result = result.max(dim=d, keepdim=True).values
+    return result
+
+
 class SM3(Optimizer):
     """Implements SM3 algorithm.
     It has been proposed in `Memory-Efficient Adaptive Optimization`_.
@@ -131,69 +192,6 @@ class SM3(Optimizer):
         else:
             # No need to compare - nu_max is bigger because of grad ** 2
             acc.copy_(nu_max)
-
-
-def _compute_sparse_update(beta, acc, grad_values, grad_indices):
-    # In the sparse case, a single accumulator is used.
-    update_values = torch.gather(acc, 0, grad_indices[0])
-    if beta > 0.:
-        update_values.mul_(beta)
-    update_values.addcmul_(grad_values, grad_values, value=1. - beta)
-    return update_values
-
-
-def _compute_update(beta, acc_list, grad):
-    rank = len(acc_list)
-    update = acc_list[0].clone()
-    for i in range(1, rank):
-        # We rely on broadcasting to get the proper end shape.
-        update = torch.min(update, acc_list[i])
-    if beta > 0.:
-        update.mul_(beta)
-    update.addcmul_(grad, grad, value=1. - beta)
-
-    return update
-
-
-def _key(i):
-    # Returns key used for accessing accumulators
-    return 'accumulator_' + str(i)
-
-
-def _add_initial_accumulators(state, grad):
-    # Creates initial accumulators. For a dense tensor of shape (n1, n2, n3),
-    # then our initial accumulators are of shape (n1, 1, 1), (1, n2, 1) and
-    # (1, 1, n3). For a sparse tensor of shape (n, *), we use a single
-    # accumulator of shape (n,).
-    shape = grad.shape
-    rank = len(shape)
-    defaults = {'device': grad.device, 'dtype': grad.dtype}
-    acc = {}
-
-    if grad.is_sparse:
-        acc[_key(0)] = torch.zeros(shape[0], **defaults)
-    elif rank == 0:
-        # The scalar case is handled separately
-        acc[_key(0)] = torch.zeros(shape, **defaults)
-    else:
-        for i in range(rank):
-            acc_shape = [1] * i + [shape[i]] + [1] * (rank - 1 - i)
-            acc[_key(i)] = torch.zeros(acc_shape, **defaults)
-
-    state.update(acc)
-
-
-def _max_reduce_except_dim(tensor, dim):
-    # Computes max along all dimensions except the given dim.
-    # If tensor is a scalar, it returns tensor.
-    rank = len(tensor.shape)
-    result = tensor
-    if rank > 0:
-        assert dim < rank
-        for d in range(rank):
-            if d != dim:
-                result = result.max(dim=d, keepdim=True).values
-    return result
 
 
 # Copyright (c) Facebook, Inc. and its affiliates.
